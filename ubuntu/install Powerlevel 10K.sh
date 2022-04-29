215f81d4767be8c6efb825e94ccaafb44e879113
Walison Magalhães <waliwalison@gmail.com>
2022-04-29 12:02:47 -0300

    Update install Powerlevel 10K.sh

--- ubuntu/install Powerlevel 10K.sh
+++ ubuntu/install Powerlevel 10K.sh
@@ -2,3 +2,4 @@ echo "Install Powerlevel 10K"
 sudo apt install git -y
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+chsh -s /bin/zsh
